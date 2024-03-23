ad4df020761dd7fb2d2d4c4b126aa4a62ec29e33
dwony <interkomon@gmail.com>
2024-03-23 12:43:26 +0500

    first commit

---
+++ pract10/pract10/pract10.cpp
@@ -0,0 +1,111 @@
+﻿// pract10.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <windows.h>
+#include <conio.h>
+#include <iostream>
+
+
+
+    //wchar_t lpszAppName[] = L"C:\\Program Files\\Windows NT\\Accessories\\wordpad.exe";
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    char lpzComline[80];
+
+   // wchar_t lpszAppName[] = L"C:\\Users\\Andrey\\Desktop\\pract10new\\x64\\Debug\\pract10new.exe";
+    STARTUPINFO si;
+    PROCESS_INFORMATION piApp;
+
+    HANDLE hProcess;
+    //дескриптор наследования
+    HANDLE InheritProcess ;
+
+    hProcess = GetCurrentProcess();
+
+    int number;
+    std::cout << "Введите число для передачи: ";
+    std::cin >> number;
+   
+    if (!DuplicateHandle(GetCurrentProcess(),hProcess, GetCurrentProcess(),  &InheritProcess,  0,TRUE,DUPLICATE_SAME_ACCESS)) 
+    {
+        _cputs("The pseudodescriptor is not created(((\n");
+        _cputs("Check a name of the process\n");
+        _cputs("Press any key to finish.\n");
+        _getch();
+        return 0;
+    }
+    std::cout << "Duplicate pseudodescreptors create." << std::endl;
+
+    _getch();
+
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+
+    wsprintf(lpzComline,"C:\\Users\\Andrey\\Desktop\\pract10new\\x64\\Debug\\pract10new.exe %d", &InheritProcess);
+
+
+    if (!CreateProcess(NULL, lpzComline, NULL, NULL, FALSE,
+        CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp))
+    {
+        
+        
+        _cputs("The new process is not created\n");
+        _cputs("Check a name of the process\n");
+        _cputs("Press any key to finish\n");
+        _getch();
+        return 0;
+    }
+   
+        _cputs("The new process is created.\n");
+        std::cout << "PID: " << piApp.dwProcessId << std::endl;
+        std::cout << "handle: " << piApp.hProcess << std::endl;
+
+        Sleep(5000);
+        while (true)
+        {
+            char c;
+            _cputs("Input 't' to terminate the new console process: ");
+            c = _getch();
+            if (c == 't')
+            {
+                _cputs("t\n");
+               
+                bool a = TerminateProcess(piApp.hProcess, 1);
+                if (!a)
+                {
+                    _cputs("Failed to terminate process\n");
+                }
+                else
+                {
+                    _cputs("Process has been completed successfully\n");
+                }
+                break;
+            }
+        }
+       
+        WaitForSingleObject(piApp.hProcess, INFINITE);
+
+        TerminateThread(piApp.hThread, 0);
+        TerminateProcess(piApp.hProcess, 0);
+
+     
+        CloseHandle(piApp.hProcess);
+        CloseHandle(piApp.hThread);
+
+       
+
+        return 0;
+}
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ pract10new/pract10new/pract10new.cpp
@@ -0,0 +1,150 @@
+﻿// pract10new.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <windows.h>
+#include <conio.h>
+#include <iostream>
+#include <math.h>
+
+//6
+DWORD WINAPI ThreadFlow1(LPVOID lpParam) {
+    std::cout << "1 поток.\n";
+
+    int num1 = 5;
+    std::cout << "Число 5 которое умножиться на 9 \n";
+    
+    int result = num1 * 9;
+
+
+    std::cout << "Результат " << result << std::endl;
+    _getch();
+    return 0;
+}
+
+DWORD WINAPI ThreadFlow2(LPVOID lpParam) {
+    std::cout << "2 поток.\n";
+    double num2 = 8;
+    std::cout << "Число 8  которое  поделиться на 3 \n";
+    
+    double result = num2 / 3;
+
+
+    std::cout << "Результат " << result << std::endl;
+    
+    _getch();
+    return 0;
+}
+DWORD WINAPI ThreadFlow3(LPVOID lpParam) {
+    std::cout << "3 поток.\n";
+
+    int num3 = 7;
+    std::cout << "Число 7  в  степени 5 \n";
+    
+    int result = pow(num3, 5);
+
+
+    std::cout << "Результат " << result << std::endl;
+    _getch();
+    return 0;
+
+   
+}
+
+
+
+int main(int argc, char* argv[])
+{
+    PROCESS_INFORMATION piApp;
+    setlocale(LC_ALL, "rus");
+    int valueReceived;
+
+    
+
+    valueReceived = atoi(argv[1]);
+    std::cout << "Значение которое было передано: " << valueReceived << std::endl;
+
+   
+
+
+   
+    int result = valueReceived * 9 / 3;
+     std::cout << "Результат: " <<result << std::endl;
+     _getch();
+
+     HANDLE hThread1 = NULL;
+     HANDLE hThread2 = NULL;
+     HANDLE hThread3 = NULL;
+     while (true) {
+         std::cout << "6 и 7 задания:" << std::endl;
+         int choice;
+         std::cout << "Что хотите сделать?: \n ";
+         std::cout << "Нажмите -1- если хотите создать 1 поток \n ";
+         std::cout << "Нажмите -2- если хотите создать 2 поток \n";
+         std::cout << "Нажмите -3- если хотите создать 3 поток  \n ";
+         std::cin >> choice;
+
+         if (choice == 1)
+         {
+             if (hThread1 == NULL) {
+                 std::cout << "Поток 1 создался  \n";
+               
+                 hThread1 = CreateThread(NULL, 0, ThreadFlow1, NULL, 0, NULL);
+                 WaitForSingleObject(hThread1, 1000);
+                 _getch();
+                
+                 TerminateThread(hThread1, 0);
+                 std::cout << " Поток 1 завершился  \n";
+             }
+         }
+
+         else if (choice == 2)
+         {
+             if (hThread2 == NULL) {
+                 std::cout << "Поток 2 создался  \n";
+                 hThread2 = CreateThread(NULL, 0, ThreadFlow2, NULL, 0, NULL);
+                 WaitForSingleObject(hThread2, 1000);
+                 _getch();
+                 
+                 TerminateThread(hThread2, 0);
+                 
+                 std::cout << "  Поток 2 завершился  \n";
+             }
+         }
+         else if (choice == 3)
+         {
+             if (hThread3 == NULL) {
+                 std::cout << "Поток 3 создался  \n";
+                 hThread3 = CreateThread(NULL, 0, ThreadFlow3, NULL, 0, NULL);
+                 WaitForSingleObject(hThread3, 1000);
+                 _getch();
+                 
+                 TerminateThread(hThread3, 0);
+                 std::cout << "Поток 3 завершился  \n";
+             }
+         }
+         else
+         {
+             std::cout << "Увы, возможно создать только 3" << std::endl;
+         }
+     }
+
+
+
+    _getch();
+    return 0;
+}
+
+
+
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
